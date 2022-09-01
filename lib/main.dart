f97a8921d3b11f08ad0dc88c3da540bbc32807eb
kerimkullov <libertyrx98@gmail.com>
2022-09-01 16:34:48 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My page'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                color: Colors.green,
+                height: 100,
+                width: 100,
+              ),
+              Container(
+                color: Colors.yellow,
+                height: 100,
+                width: 100,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
